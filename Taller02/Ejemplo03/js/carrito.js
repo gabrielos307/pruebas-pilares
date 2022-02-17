456a2d713aaa57db24036ebd6b85d180d12f4ba8
ghcib1 <cib1@ai.patronato.unam.mx>
2022-02-16 19:21:21 -0600

    función agregar funcionando correctamente

--- Taller02/Ejemplo03/js/carrito.js
+++ Taller02/Ejemplo03/js/carrito.js
@@ -1,12 +1,26 @@
 var carrito = [];
+var bandera = 0;
 function agregar(producto){
-   
-    producto.push(1); //[id, nombre, precio, cantidad*]
+  if(this.carrito.length!=0){
+    for(let i = 0; i<this.carrito.length; i++ ){
+      if(this.carrito[i][0] == producto[0]){
+        this.carrito[i][3] = this.carrito[i][3] + 1;
+        bandera = 1;
+        break;
+        // a = a + 1 
+      }
+    }
+    if(bandera == 0){
+      producto.push(1);
+      this.carrito.push(producto);
     
-    this.carrito.push(producto);
-        
-    
-    console.log(this.carrito);
+    }
+    bandera = 0;
+  }else{
+    producto.push(1);
+    this.carrito.push(producto); 
+  }
+  console.log(this.carrito);
     
 }
 
@@ -17,11 +31,9 @@ function eliminar(producto){
 
 
 function listar() {
+    texto = "";
     for(let i = 0; i < this.carrito.length; i++){
-      '<h1>'
-        +carrito[0][1]
-      '</h1>'
-        document.getElementById("titulo").innerHTML = carrito[i][1];
-        document.getElementById("precio").innerHTML = carrito[i][2];
+      texto += '<h1>'+carrito[i][1]+' x'+carrito[i][3]+'</h1>'; 
     }
+    document.getElementById("titulo").innerHTML = texto;
 }
